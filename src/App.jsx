25d2b6907433d898a2735b8f1760268f33918794
cryptojang <robert.crypto.jang@gmail.com>
2023-11-15 10:06:07 +0900

    layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 const App = () => {
   return (
-    <div className="bg-red-100">
-      <div>Chat GPT</div>
+    <div className="bg-red-100 min-h-screen max-w-screen-md mx-auto flex">
+      <div className="bg-blue-100 w-52">Side</div>
+      <div className="bg-green-100 grow flex flex-col">
+        <div className="bg-yellow-100 grow">Main</div>
+        <div className="bg-purple-100 h-24">Chat Bar</div>
+      </div>
     </div>
   );
 };
